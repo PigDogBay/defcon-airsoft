891c798d2858281ba8058b99f8919938081323bc
Mark Bailey <pigdogbay@yahoo.co.uk>
2015-07-18 12:04:35 +0100

    Moved data logic for speed calcs into main model

--- app/src/main/java/uk/co/defconairsoft/muzzlevelocitycalculator/MainFragment.java
+++ app/src/main/java/uk/co/defconairsoft/muzzlevelocitycalculator/MainFragment.java
@@ -5,13 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import uk.co.defconairsoft.muzzlevelocitycalculator.model.AudioMonitor;
-import uk.co.defconairsoft.muzzlevelocitycalculator.model.Ballistics;
 import uk.co.defconairsoft.muzzlevelocitycalculator.model.LiveAnalysis;
 import uk.co.defconairsoft.muzzlevelocitycalculator.model.MainModel;
 
@@ -19,7 +14,7 @@ import uk.co.defconairsoft.muzzlevelocitycalculator.model.MainModel;
  * Created by Mark on 04/07/2015.
  */
 public class MainFragment extends Fragment implements LiveAnalysis.IAnalysisListener {
-    TextView resultsText;
+    TextView speedText,statsText;
     MainModel mainModel;
 
     public MainFragment(){
@@ -28,13 +23,14 @@ public class MainFragment extends Fragment implements LiveAnalysis.IAnalysisList
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_monitor, container, false);
         wireUpControls(rootView);
         return rootView;
     }
 
     private void wireUpControls(View view){
-        resultsText = (TextView)view.findViewById(R.id.resultsText);
+        speedText = (TextView)view.findViewById(R.id.monitorSpeed);
+        statsText = (TextView)view.findViewById(R.id.monitorStats);
     }
 
     @Override
@@ -42,29 +38,36 @@ public class MainFragment extends Fragment implements LiveAnalysis.IAnalysisList
         super.onResume();
         this.mainModel = ((MainActivity)getActivity()).getMainModel();
         this.mainModel.setListener(this);
-  //      this.mainModel.start();
+        this.mainModel.start();
     }
 
     @Override
     public void onPause() {
         super.onPause();
         this.mainModel.setListener(null);
-    //    this.mainModel.stop();
+        this.mainModel.stop();
     }
 
-    int count =0;
+    private void modelToView(){
+        speedText.setText(String.format("%.1f fps", mainModel.getSpeed()));
+        StringBuilder sbuff = new StringBuilder();
+        sbuff.append(String.format("Count %d\n",mainModel.getCount()));
+        sbuff.append(String.format("Average %.1f fps\n",mainModel.getAverage()));
+        sbuff.append(String.format("Previous 1. %.1f fps\n",mainModel.getPrevious()[0]));
+        sbuff.append(String.format("Previous 2. %.1f fps\n",mainModel.getPrevious()[1]));
+        sbuff.append(String.format("Previous 3. %.1f fps\n",mainModel.getPrevious()[2]));
+        sbuff.append(String.format("Measured Time %.3f s\n",mainModel.getTimeBetweenPeaks()));
+
+        statsText.setText(sbuff.toString());
+    }
+
     @Override
     public void onPelletFired(final double durationOfFlight) {
         getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                count++;
-                double speed = 0d;
-                if (durationOfFlight>0.05D) {
-                    speed = mainModel.getBallistics().calculateMuzzleVelocity(0D, durationOfFlight);
-                    speed = mainModel.getBallistics().convertToFeetPerSecond(speed);
-                }
-                resultsText.setText(String.format("%d: %.3f s / %.1f fps",count, durationOfFlight,speed));
+                mainModel.calculateSpeed(durationOfFlight);
+                modelToView();
             }
         });
     }
--- app/src/main/java/uk/co/defconairsoft/muzzlevelocitycalculator/model/MainModel.java
+++ app/src/main/java/uk/co/defconairsoft/muzzlevelocitycalculator/model/MainModel.java
@@ -1,26 +1,45 @@
 package uk.co.defconairsoft.muzzlevelocitycalculator.model;
 
-import android.util.Log;
-
 /**
  * Created by Mark on 04/07/2015.
  */
 public class MainModel
 {
-    private String results="";
+    public static final double MIN_TIME = 0.05D;
     private AudioMonitor audioMonitor;
     private Ballistics ballistics;
+    private int count;
+    private double speed;
+    private double average;
+    private double timeBetweenPeaks;
+    private double[] previous = new double[3];
 
-
-    public String getResults() {
-        return results;
+    public int getCount() {
+        return count;
+    }
+    public void setCount(int count) {
+        this.count = count;
+    }
+    public double getSpeed() {
+        return speed;
+    }
+    public double getAverage() {
+        return average;
+    }
+    public void setAverage(double average) {
+        this.average = average;
+    }
+    public double[] getPrevious() {
+        return previous;
     }
     public Ballistics getBallistics(){
         return ballistics;
     }
-
     public void setListener(LiveAnalysis.IAnalysisListener listener){
         audioMonitor.getLiveAnalysis().setListener(listener);
+    }
+    public double getTimeBetweenPeaks() {
+        return timeBetweenPeaks;
     }
 
 
@@ -42,4 +61,23 @@ public class MainModel
             e.printStackTrace();
         }
     }
+
+    public void calculateSpeed(double timeBetweenPeaks){
+        this.timeBetweenPeaks = timeBetweenPeaks;
+        count++;
+        previous[2]=previous[1];
+        previous[1]=previous[0];
+        previous[0]=speed;
+        speed = 0d;
+        if (timeBetweenPeaks> MIN_TIME) {
+            speed = getBallistics().calculateMuzzleVelocity(0D, timeBetweenPeaks);
+            speed = getBallistics().convertToFeetPerSecond(speed);
+        }
+
+        average = speed+previous[0]+previous[1]+previous[2];
+        average = average/4;
+
+    }
+
+
 }
